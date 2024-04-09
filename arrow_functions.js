486d9abcaff1ba994be265f1ce73cf81a31037eb
KayongoYongo <sknsammie10@gmail.com>
2024-04-09 11:03:32 +0300

    add arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,14 @@
+// Normal function
+const add = function (x, y) {
+    return x + y;
+}
+
+// Arrow function
+// The function keyword is not being used
+// ES6 standard
+const addition = (x, y) => {
+    return x + y;
+}
+
+console.log(add(2,3))
+console.log(addition(4,6))
