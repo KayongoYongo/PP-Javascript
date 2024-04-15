4f3f7c5dbf0020f171c5519b6da815a8bbbe095b
KayongoYongo <sknsammie10@gmail.com>
2024-04-15 16:44:12 +0300

    select elements update

--- JavaScript-DOM-Manipulation/select_elements.js
+++ JavaScript-DOM-Manipulation/select_elements.js
@@ -1,26 +1,26 @@
 // Selecting element using ID
-const title = document.getElementById('main-heading');
+const mainTitle = document.getElementById('main-heading');
 
-console.log(title);
+// console.log(title);
 
 // Selecting element using class name
-const listItems = document.getElementsByClassName('list-items');
+const mainListItems = document.getElementsByClassName('list-items');
 
-console.log(listItems);
+// console.log(listItems);
 
 // Selecting elements using a tag name
 const listTag = document.getElementsByTagName('li');
 
-console.log(listTag);
+// console.log(listTag);
 
 // querySelector()
 // The advantage of this method is that it accepts all CSS styles
 // It selects the first instance of a method
 const container = document.querySelector('div');
 
-console.log(container);
+// console.log(container);
 
 // querySelectorAll()
 const listItemsList = document.querySelectorAll('li');
 
-console.log(listItemsList);
+// console.log(listItemsList);
