6f68104aca2ffbcb015a08aee8c845b70704ee87
KayongoYongo <sknsammie10@gmail.com>
2024-04-15 16:43:53 +0300

    styling elements update

--- JavaScript-DOM-Manipulation/styling_elements.js
+++ JavaScript-DOM-Manipulation/styling_elements.js
@@ -9,7 +9,7 @@ console.log(title);
 const listItems = document.querySelectorAll('.list-items');
 
 for( i = 0 ; i < listItems.length ; i++ ) {
-    listItems[i].style.fontSize = '5rem';
+    listItems[i].style.fontSize = '2rem';
 }
 
 console.log(listItems);
