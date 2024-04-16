185fdb50858593fe79daf05accd67fc6efb726d4
KayongoYongo <sknsammie10@gmail.com>
2024-04-16 18:29:45 +0300

    create elements

--- JavaScript-DOM-Manipulation/creating_elements.js
+++ JavaScript-DOM-Manipulation/creating_elements.js
@@ -1,3 +1,4 @@
+// Select the list
 const ul = document.querySelector('ul');
 
 // Creating elements
@@ -7,5 +8,19 @@ const li = document.createElement('li');
 ul.append(li);
 
 // Modifying the text
+const firstListItem = document.querySelector('.list-items');
 
-c
+li.innerText = 'X-Men';
+
+// Modifying attributes and classes
+li.setAttribute('id', 'main-heading');
+
+// Remove attribute
+li.removeAttribute('id');
+
+const title = document.querySelector('#main-heading');
+
+console.log(title.getAttribute('id'));
+
+// Remove elements
+li.remove();
