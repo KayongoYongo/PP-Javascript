b72c8ec3ec6f999a3721d1fee719b2eaf01105c3
KayongoYongo <sknsammie10@gmail.com>
2024-05-03 20:45:22 +0300

    edit on navbar js

--- JavaScript-Projects/Portfolio-Website/navbar.js
+++ JavaScript-Projects/Portfolio-Website/navbar.js
@@ -3,10 +3,6 @@ let menuBar = document.getElementById('showbar');
 let hideBar = document.getElementById('hidebar');
 let sideBar = document.querySelector('.sidebar');
 
-console.log(menuBar);
-console.log(sideBar);
-console.log(hideBar);
-
 function showSideBar() {
     sideBar.style.display = 'flex';
 };
